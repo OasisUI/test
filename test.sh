52e5e529d09305475ab978e7321114010f6b363a
joe223 <heyin223@163.com>
2018-07-18 14:59:40 +0800

    update travis config

--- test.sh
+++ test.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env sh
 
+$VESION = $(npm version patch)
 echo 'TEST_VALUE:' $TEST_VALUE;
 rm -rf web CI-IN-ACTION
 mkdir web
 cd web
-git clone -b dev https://$CI_TOKEN@github.com/joe223/CI-IN-ACTION.git .
+git clone -b master https://$CI_TOKEN@github.com/joe223/CI-IN-ACTION.git .
 npm run build
+
 cd ..
 git clone -b gh-pages https://$CI_TOKEN@github.com/joe223/CI-IN-ACTION.git
 
 cp -rf web/* ./CI-IN-ACTION
 cd CI-IN-ACTION
 git add .
-git commit -m "build"
+git commit -m "build $VESION"
+
 git push origin gh-pages
